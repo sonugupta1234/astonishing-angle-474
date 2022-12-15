355ece05af875ef0c71bd334bf18629b708b1f0b
sonugupta1234 <sonuguptahld037@gmail.com>
2022-12-16 00:21:28 +0530

    homepageadded

--- rct-101/src/App.js
+++ rct-101/src/App.js
@@ -1,12 +1,14 @@
 import Home from "./Pages/Homepage"
 import './App.css';
+import AllRoutes from "./Routes/AllRoutes"
 
 
 function App() {
   return (
+    
     <div className="App">
       <Home />
-      
+    <AllRoutes />
     </div>
   );
 }
--- rct-101/src/Pages/Homepage.jsx
+++ rct-101/src/Pages/Homepage.jsx
@@ -1,13 +1,27 @@
 import Navbar from "../Components/Navbar"
+import Searchbar_Homepage from "../Components/Searchbar_Homepage";
+import Top_Destination from "../Components/Top_Destination"
+import { Text } from "@chakra-ui/react";
+import BodyPart_HomePage from "../Components/BodyPart_Homepage";
+import Vacation_Idea from "../Components/VacationIdeas_Homepage";
+import Footer from "../Components/Footer";
 
 
 function Home(){
+
+   
+
+    
     return (
         <>
         <Navbar />
-        <div>
-            <img style={{height: "500px", width: "100%"}} src="https://d2v8elt324ukrb.cloudfront.net/static/new_template/media/Pimal2-1.2d5d237dbd32.jpg" alt=""/>
-        </div>
+        <Searchbar_Homepage /> 
+        <Text fontSize="lg"  position={'absolute'} ml={7} mt={3}>Top Destinations</Text>
+        <Top_Destination />
+        <BodyPart_HomePage />
+        <Text fontSize="lg"  position={'absolute'} ml={7} mt={3}>Vacation Ideas</Text>
+        <Vacation_Idea />
+        <Footer />
         </>
     )
 }
